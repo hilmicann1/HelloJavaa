dc9290660feb41b1f40228060c88acf61bbeff74
Hilmi Can <canhilmi1996@gmail.com>
2022-10-12 14:20:44 +0300

    first commit1

--- arraylistDemo/src/Main.java
+++ arraylistDemo/src/Main.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
+
 public class Main  {
 
     public static void main(String[] args){
-        int[] sayilar = new int[]{1,2,3};
-        sayilar = new int[4];
-        System.out.println(sayilar[0]);
+        ArrayList sayilar = new ArrayList();
+        System.out.println(sayilar.size());
+        sayilar.add(1);
+        sayilar.add(10);
+        sayilar.add("Ankara");
+        //System.out.println(sayilar.size());
+
+        //sayilar.set(2,100);
+
+        //sayilar.remove(0);
+        //sayilar.clear();
+        //System.out.println(sayilar.get(0));
+
+        for (Object i:sayilar){
+            System.out.println(i);
+
+        }
 
     }
 }

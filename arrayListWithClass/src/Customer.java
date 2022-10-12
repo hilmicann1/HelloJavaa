0241daa62d2be9fe703aadb225c53fd5881ee44c
Hilmi Can <canhilmi1996@gmail.com>
2022-10-12 14:27:07 +0300

    first commit2

---
+++ arrayListWithClass/src/Customer.java
@@ -0,0 +1,16 @@
+public class Customer {
+    int id;
+    String firstname;
+    String lastname;
+
+    public Customer(int i, String s){
+
+    }
+
+    public Customer(int id, String firstname, String lastname){
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+
+    }
+}
